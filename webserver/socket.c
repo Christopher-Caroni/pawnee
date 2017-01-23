34ffc80cc91198c00384594cd416883d59f32eb6
Christopher CARONI <christopher.caroni@etudiant.univ-lille1.fr>
2017-01-23 16:39:00 +0100

    socket.c and socket.h

---
+++ webserver/socket.c
@@ -0,0 +1,27 @@
+int creer_serveur(int port) {
+  int socket_serveur;
+  socket_serveur = socket (AF_INET, SOCK_STREAM, 0);
+  if ( socket_serveur == -1)
+  {
+    perror("socket_serveur");
+    return -1;
+  }
+
+  struct sockaddr_in saddr ;
+  saddr.sin_family = AF_INET ; /* Socket ipv4 */
+  saddr.sin_port = htons(port); /* Port d ’é coute */
+  saddr.sin_addr.s_addr = INADDR_ANY ; /* é coute sur toutes les interfaces */
+
+  if ( bind (socket_serveur, ( struct sockaddr *)& saddr, sizeof(saddr)) == -1)
+  {
+    perror("bind socker_serveur");
+    return -1;
+  }
+
+  if ( listen(socket_serveur, 10) == -1)
+  {
+    perror("listen socket_serveur");
+    return -1;
+  }
+  return socket_serveur;
+}
