f3954c944bcb4523dde3bd477e4ca8943a8b0c4c
Christopher CARONI <christopher.caroni@etudiant.univ-lille1.fr>
2017-01-23 17:26:29 +0100

    serveur basique

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,46 @@
 # include <stdio.h>
 # include <string.h>
-int main (int argc, char ** argv)
+# include <errno.h>
+#include <unistd.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include "socket.h"
+
+int main (void)
 {
-  /* Arnold Robbins in the LJ of February ’95 , describing RCS */
-  if ( argc > 1 && strcmp (argv [1] , "-advice") == 0) {
-    printf ("Don’t Panic !\n");
-    return 42;
+  while (1)
+  {
+    int socket_serveur = creer_serveur(8080);
+    int socket_client = accept(socket_serveur, NULL, NULL);
+    if ( socket_client == -1)
+    {
+      perror("accept");
+    }
+    char* message = "German: Hallo! Willkommen Kuruk Server von Nicolas de Moraes und Christopher Caroni erstellt.\n"
+    "English: Hello! Welcome to the Kuruk server created by Nicolas De Moraës and Christopher Caroni.\n"
+    "Spanish: ¡Hola! servidor Kuruk Bienvenido creado por Nicolás de Moraes y Christopher Caroni.\n"
+    "French: Bonjour! Bienvenue sur le serveur Kuruk créé par Nicolas De Moraës et Christopher Caroni.\n"
+    "Hindi: नमस्कार! आपका स्वागत है Kuruk सर्वर निकोलस डे मोरेस और क्रिस्टोफर Caroni द्वारा बनाई गई।\n"
+    "Italian: Buongiorno! server di benvenuto Kuruk creato da Nicolas de Moraes e Christopher Caroni.\n"
+    "Japanese: こんにちは！ニコラス・デ・モラエスとクリストファー・カロニによって作成されたウェルカムKurukサーバー\n"
+    "Norwegian: Hello! Velkommen Kuruk server opprettet av Nicolas de Moraes og Christopher Caroni.\n"
+    "Polish: Cześć! Witamy serwer Kuruk stworzony przez Nicolasa de Moraes i Christopher Caroni.\n"
+    "Portuguese: Olá! servidor Kuruk Bem-vindo criado por Nicolas de Moraes e Christopher Caroni.\n";
+    if (write(socket_client, message, strlen(message)) == -1) {
+      perror("write message");
+      return -1;
+    }
+
+    int connected = 1;
+    while (connected)
+    {
+      char readMessage[1024];
+      connected = read(socket_client, &readMessage, 1024);
+      write(socket_client, &readMessage, connected);
+    }
+    /* Une fois qu'on lit rien, on ferme les sockets */
+    close(socket_client);
+    close(socket_serveur);
   }
-  printf ("Need an advice ?\n");
   return 0;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,3 +1,9 @@
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <errno.h>
+# include <stdio.h>
+#include <arpa/inet.h>
+
 int creer_serveur(int port) {
   int socket_serveur;
   socket_serveur = socket (AF_INET, SOCK_STREAM, 0);
@@ -9,10 +15,10 @@ int creer_serveur(int port) {
 
   struct sockaddr_in saddr ;
   saddr.sin_family = AF_INET ; /* Socket ipv4 */
-  saddr.sin_port = htons(port); /* Port d ’é coute */
-  saddr.sin_addr.s_addr = INADDR_ANY ; /* é coute sur toutes les interfaces */
+  saddr.sin_port = htons(port); /* Port d ’écoute */
+  saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
 
-  if ( bind (socket_serveur, ( struct sockaddr *)& saddr, sizeof(saddr)) == -1)
+  if ( bind(socket_serveur, ( struct sockaddr *)& saddr, sizeof(saddr)) == -1)
   {
     perror("bind socker_serveur");
     return -1;
