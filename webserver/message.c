152b19c6116ba10887d93dba0d0e4374fb50ae28
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2017-02-11 17:35:42 +0100

    regex get request

--- webserver/message.c
+++ webserver/message.c
@@ -64,6 +64,7 @@ int read_and_write(FILE* fp) {
   return 1;
 }
 
+
 /*
 * read un message jusqua 1024 de longueur et réécrit ce message
 * Lance perror en cas d'erreur. Stocke dans connected la valeur de
@@ -84,6 +85,39 @@ int repeat_messages(int socket_client) {
   return 0;
 }
 
+int match_http_request(char *str_request) {
+  int match_result;
+  int compile_result;
+  regex_t compiled_regex;
+  regmatch_t pmatch[4]; // We have 3 capturing groups + the whole match_result group
+  size_t nb_matches = 4; // Same as above
+
+  const char *str_regex = "^(GET) (/.*)+ (HTTP/1[.][0-1])+\n$";
+  compile_result = regcomp(&compiled_regex, str_regex, REG_EXTENDED);
+  if (compile_result == 0)
+  {
+    match_result = regexec(&compiled_regex, str_request, nb_matches, pmatch, 0);
+    nb_matches = compiled_regex.re_nsub;
+    regfree(&compiled_regex);
+    if (match_result == 0)
+    {
+      printf("matched HTTP GET request\n");
+      // pmatch[x].rm_so = offset
+      // pmatch[x].rm_eo = end offset
+      // printf(.*) -> specify length of string to print instead of whole string
+      printf("request protocol : \"%.*s\"\n", (int) (pmatch[1].rm_eo - pmatch[1].rm_so), &str_request[pmatch[1].rm_so]);
+      printf("requested ressource : \"%.*s\"\n", (int) (pmatch[2].rm_eo - pmatch[2].rm_so), &str_request[pmatch[2].rm_so]);
+      printf("http version : \"%.*s\"\n", (int) (pmatch[3].rm_eo - pmatch[3].rm_so), &str_request[pmatch[3].rm_so]);
+      return 0;
+    } else if (match_result == REG_NOMATCH)
+    {
+      printf("Did not match HTTP GET request\n");
+      return -1;
+    }
+  }
+  return -1;
+}
+
 int treatHTTP(int socket_client) {
   FILE * fp = fdopen(socket_client, "w+");
   char input[1024];
@@ -92,21 +126,7 @@ int treatHTTP(int socket_client) {
     return -1;
   }
 
-  regex_t regex;
-  int reti;
+  printf("message received : \"%s\"\n", input);
 
-  reti = regcomp(&regex, "GET\\s\\/\\sHTTP\\/[1].[0-1]", 0);
-  if (reti) {
-    fprintf(stderr, "Could not compile regex\n");
-    return -1;
-  }
-
-  reti = regexec(&regex, input, 0, NULL, 0);
-  if (!reti) {
-    printf("matched\n");
-  }
-  else if (reti == REG_NOMATCH) {
-    printf("did not match\n");
-  }
-  return 0;
+  return match_http_request(input);
 }
