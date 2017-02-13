63761a877c781e4dad8b019c3cde888f40bb7ee7
Christopher CARONI <christopher.caroni@etudiant.univ-lille1.fr>
2017-02-13 15:43:40 +0100

    simple get verifications and response

--- webserver/message.c
+++ webserver/message.c
@@ -40,9 +40,14 @@ const char* WELCOME_MESSAGE =
 "Polish: Cześć! Witamy serwer Kuruk stworzony przez Nicolasa de Moraes i Christopher Caroni.\n"
 "Portuguese: Olá! servidor Kuruk Bem-vindo criado por Nicolas de Moraes e Christopher Caroni.\n";
 
-int display_welcome_message(int socket_client) {
-  if (write(socket_client, WELCOME_MESSAGE, strlen(WELCOME_MESSAGE)) == -1) {
-    perror("Cannot write welcome message");
+int display_welcome_message(FILE * fp) {
+  char* response = "HTTP/1.1 200 OK\n"
+  "Connection: close\n"
+  "Content-Length: 3777\n"
+  "\n";
+  if (fprintf(fp, "%s%s", response, WELCOME_MESSAGE) < 0)
+  {
+    perror("fprintf");
     return -1;
   }
   return 0;
@@ -85,14 +90,14 @@ int repeat_messages(int socket_client) {
   return 0;
 }
 
-int match_http_request(char *str_request) {
+int match_get_request(char *str_request) {
   int match_result;
   int compile_result;
   regex_t compiled_regex;
   regmatch_t pmatch[4]; // We have 3 capturing groups + the whole match_result group
   size_t nb_matches = 4; // Same as above
 
-  const char *str_regex = "^(GET) (/.*)+ (HTTP/1[.][0-1])+\n$";
+  const char *str_regex = "(GET) (/.*)+ (HTTP/1[.][0-1])\r\n";
   compile_result = regcomp(&compiled_regex, str_regex, REG_EXTENDED);
   if (compile_result == 0)
   {
@@ -118,15 +123,65 @@ int match_http_request(char *str_request) {
   return -1;
 }
 
+int match_empty_line(char* str_request) {
+  int match_result;
+  int compile_result;
+  regex_t compiled_regex;
+
+  const char *str_regex = "^(\n|\r\n)";
+  compile_result = regcomp(&compiled_regex, str_regex, REG_EXTENDED);
+  if (compile_result == 0)
+  {
+    match_result = regexec(&compiled_regex, str_request, 0, NULL, 0);
+    regfree(&compiled_regex);
+    if (match_result == 0)
+    {
+      printf("matched empty line\n");
+      return 0;
+    } else if (match_result == REG_NOMATCH)
+    {
+      return -1;
+    }
+  }
+  return -1;
+}
+
+int send_bad_request(FILE *fp) {
+  char* bad_request_message = "HTTP/1.1 400 Bad Request\n"
+  "Connection: close\n"
+  "Content-Length: 17\n"
+  "\n"
+  "400 Bad request\n";
+  if (fprintf(fp, "%s", bad_request_message) < 0)
+  {
+    perror("fprintf");
+    return -1;
+  }
+  return 0;
+}
+
+
 int treatHTTP(int socket_client) {
   FILE * fp = fdopen(socket_client, "w+");
   char input[1024];
-  if ( fgets(input, 1024, fp) == NULL) {
-    perror("fgets");
-    return -1;
+  int lineCount = 0;
+
+  while (fgets(input, 1024, fp) != NULL)
+  {
+    printf("message received : \"%s\"\n", input);
+    if (lineCount == 0) {
+      if (match_get_request(input) == -1) {
+        send_bad_request(fp);
+        return -1;
+      }
+    } else if (lineCount > 0) {
+      if (match_empty_line(input) == 0)
+      {
+        display_welcome_message(fp);
+        return 0;
+      }
+    }
+    lineCount++;
   }
-
-  printf("message received : \"%s\"\n", input);
-
-  return match_http_request(input);
+  return -1;
 }
