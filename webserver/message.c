81ddc46a95eb53a541f99cef61d379521d95c047
Christopher CARONI <christopher.caroni@etudiant.univ-lille1.fr>
2017-02-13 16:49:11 +0100

    erreur 404

--- webserver/message.c
+++ webserver/message.c
@@ -40,6 +40,57 @@ const char* WELCOME_MESSAGE =
 "Polish: Cześć! Witamy serwer Kuruk stworzony przez Nicolasa de Moraes i Christopher Caroni.\n"
 "Portuguese: Olá! servidor Kuruk Bem-vindo criado por Nicolas de Moraes e Christopher Caroni.\n";
 
+const char* MESSAGE_404 =
+"          _____                    _____                    _____                    _____                    _____          \n"
+"         /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\         \n"
+"        /::\\____\\                /::\\____\\                /::\\    \\                /::\\____\\                /::\\____\\       \n"
+"       /:::/    /               /:::/    /               /::::\\    \\              /:::/    /               /:::/    /        \n"
+"      /:::/    /               /:::/    /               /::::::\\    \\            /:::/    /               /:::/    /         \n"
+"     /:::/    /               /:::/    /               /:::/\\:::\\    \\          /:::/    /               /:::/    /          \n"
+"    /:::/____/               /:::/    /               /:::/__\\:::\\    \\        /:::/    /               /:::/____/         \n"
+"   /::::\\    \\              /:::/    /               /::::\\   \\:::\\    \\      /:::/    /               /::::\\    \\           \n"
+"  /::::::\\____\\________    /:::/    /      _____    /::::::\\   \\:::\\    \\    /:::/    /      _____    /::::::\\____\\________  \n"
+" /:::/\\:::::::::::\\    \\  /:::/____/      /\\    \\  /:::/\\:::\\   \\:::\\____\\  /:::/____/      /\\    \\  /:::/\\:::::::::::\\    \\ \n"
+"/:::/  |:::::::::::\\____\\|:::|    /      /::\\____\\/:::/  \\:::\\   \\:::|    ||:::|    /      /::\\____\\/:::/  |:::::::::::\\____\\\n"
+"\\::/   |::|~~~|~~~~~     |:::|____\\     /:::/    /\\::/   |::::\\  /:::|____||:::|____\\     /:::/    /\\::/   |::|~~~|~~~~~     \n"
+" \\/____|::|   |           \\:::\\    \\   /:::/    /  \\/____|:::::\\/:::/    /  \\:::\\    \\   /:::/    /  \\/____|::|   |          \n"
+"       |::|   |            \\:::\\    \\ /:::/    /         |:::::::::/    /    \\:::\\    \\ /:::/    /         |::|   |          \n"
+"       |::|   |             \\:::\\    /:::/    /          |::|\\::::/    /      \\:::\\    /:::/    /          |::|   |          \n"
+"       |::|   |              \\:::\\__/:::/    /           |::| \\::/____/        \\:::\\__/:::/    /           |::|   |        \n"
+"       |::|   |               \\::::::::/    /            |::|  ~|               \\::::::::/    /            |::|   |          \n"
+"       |::|   |                \\::::::/    /             |::|   |                \\::::::/    /             |::|   |          \n"
+"       \\::|   |                 \\::::/    /              \\::|   |                 \\::::/    /              \\::|   |          \n"
+"        \\:|   |                  \\::/____/                \\:|   |                  \\::/____/                \\:|   |          \n"
+"         \\|___|                   ~~                       \\|___|                   ~~                       \\|___|          \n"
+"\n\n"
+"EEEEEEEEEEEEEEEEEEEEEE                                                                                         444444444       000000000            444444444\n"
+"E::::::::::::::::::::E                                                                                        4::::::::4     00:::::::::00         4::::::::4  \n"
+"E::::::::::::::::::::E                                                                                       4:::::::::4   00:::::::::::::00      4:::::::::4  \n"
+"EE::::::EEEEEEEEE::::E                                                                                      4::::44::::4  0:::::::000:::::::0    4::::44::::4  \n"
+"  E:::::E       EEEEEErrrrr   rrrrrrrrr   rrrrr   rrrrrrrrr      ooooooooooo   rrrrr   rrrrrrrrr           4::::4 4::::4  0::::::0   0::::::0   4::::4 4::::4  \n"
+"  E:::::E             r::::rrr:::::::::r  r::::rrr:::::::::r   oo:::::::::::oo r::::rrr:::::::::r         4::::4  4::::4  0:::::0     0:::::0  4::::4  4::::4  \n"
+"  E::::::EEEEEEEEEE   r:::::::::::::::::r r:::::::::::::::::r o:::::::::::::::or:::::::::::::::::r       4::::4   4::::4  0:::::0     0:::::0 4::::4   4::::4  \n"
+"  E:::::::::::::::E   rr::::::rrrrr::::::rrr::::::rrrrr::::::ro:::::ooooo:::::orr::::::rrrrr::::::r     4::::444444::::4440:::::0 000 0:::::04::::444444::::444\n"
+"  E:::::::::::::::E    r:::::r     r:::::r r:::::r     r:::::ro::::o     o::::o r:::::r     r:::::r     4::::::::::::::::40:::::0 000 0:::::04::::::::::::::::4\n"
+"  E::::::EEEEEEEEEE    r:::::r     rrrrrrr r:::::r     rrrrrrro::::o     o::::o r:::::r     rrrrrrr     4444444444:::::4440:::::0     0:::::04444444444:::::444\n"
+"  E:::::E              r:::::r             r:::::r            o::::o     o::::o r:::::r                           4::::4  0:::::0     0:::::0          4::::4  \n"
+"  E:::::E       EEEEEE r:::::r             r:::::r            o::::o     o::::o r:::::r                           4::::4  0::::::0   0::::::0          4::::4  \n"
+"EE::::::EEEEEEEE:::::E r:::::r             r:::::r            o:::::ooooo:::::o r:::::r                           4::::4  0:::::::000:::::::0          4::::4  \n"
+"E::::::::::::::::::::E r:::::r             r:::::r            o:::::::::::::::o r:::::r                         44::::::44 00:::::::::::::00         44::::::44\n"
+"E::::::::::::::::::::E r:::::r             r:::::r             oo:::::::::::oo  r:::::r                         4::::::::4   00:::::::::00           4::::::::4\n"
+"EEEEEEEEEEEEEEEEEEEEEE rrrrrrr             rrrrrrr               ooooooooooo    rrrrrrr                         4444444444     000000000             4444444444\n"
+"\n\n"
+"German: Seite nicht gefunden!\n"
+"English: Page not found!\n"
+"Spanish: ¡Página no encontrada!\n"
+"French: Page non trouvée!\n"
+"Hindi: पृष्ठ नहीं मिला!\n"
+"Italian: Pagina non trovata!\n"
+"Japanese: ページが見つかりません！\n"
+"Norwegian: Side ikke funnet!\n"
+"Polish: Strona nie znaleziona!\n"
+"Portuguese: Página não encontrada!\n";
+
 int display_welcome_message(FILE * fp) {
   char* response = "HTTP/1.1 200 OK\n"
   "Connection: close\n"
@@ -51,6 +102,32 @@ int display_welcome_message(FILE * fp) {
     return -1;
   }
   return 0;
+}
+
+int display_404(FILE *fp) {
+  char* response = "HTTP/1.1 200 OK\n"
+  "Connection: close\n"
+  "Content-Length: 5766\n"
+  "\n";
+  if (fprintf(fp, "%s%s", response, MESSAGE_404) < 0)
+  {
+    perror("fprintf");
+    return -1;
+  }
+  return 0;
+}
+
+int respond(FILE * fp, char *ressource) {
+  char *base = "/";
+  printf("result comparison = %d\n", strcmp(ressource, base));
+  if (strcmp(ressource, base) == 0) {
+    display_welcome_message(fp);
+    return 0;
+  } else {
+    display_404(fp);
+    return 0;
+  }
+  return -1;
 }
 
 int read_and_write(FILE* fp) {
@@ -90,7 +167,7 @@ int repeat_messages(int socket_client) {
   return 0;
 }
 
-int match_get_request(char *str_request) {
+int match_get_request(char *str_request, char* ressource) {
   int match_result;
   int compile_result;
   regex_t compiled_regex;
@@ -110,6 +187,11 @@ int match_get_request(char *str_request) {
       // pmatch[x].rm_so = offset
       // pmatch[x].rm_eo = end offset
       // printf(.*) -> specify length of string to print instead of whole string
+
+      int startRessource = (int) (pmatch[2].rm_so);
+      int lengthRessource = (int) ( (pmatch[2].rm_eo) - (pmatch[2].rm_so) );
+      strncpy(ressource, str_request + startRessource, lengthRessource);
+
       printf("request protocol : \"%.*s\"\n", (int) (pmatch[1].rm_eo - pmatch[1].rm_so), &str_request[pmatch[1].rm_so]);
       printf("requested ressource : \"%.*s\"\n", (int) (pmatch[2].rm_eo - pmatch[2].rm_so), &str_request[pmatch[2].rm_so]);
       printf("http version : \"%.*s\"\n", (int) (pmatch[3].rm_eo - pmatch[3].rm_so), &str_request[pmatch[3].rm_so]);
@@ -165,19 +247,20 @@ int treatHTTP(int socket_client) {
   FILE * fp = fdopen(socket_client, "w+");
   char input[1024];
   int lineCount = 0;
+  char ressource[1024] = "";
 
   while (fgets(input, 1024, fp) != NULL)
   {
     printf("message received : \"%s\"\n", input);
     if (lineCount == 0) {
-      if (match_get_request(input) == -1) {
+      if (match_get_request(input, ressource) == -1) {
         send_bad_request(fp);
         return -1;
       }
     } else if (lineCount > 0) {
       if (match_empty_line(input) == 0)
       {
-        display_welcome_message(fp);
+        respond(fp, ressource);
         return 0;
       }
     }
